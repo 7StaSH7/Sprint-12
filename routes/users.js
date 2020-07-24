fe609ada9cc237382aab7880a2f9c81837de1a76
StaSH <sanchasyo@mail.ru>
2020-07-24 12:40:17 +0300

    Corrected the last comment in pull request

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ usersRouter.get('/', (req, res) => {
 
 usersRouter.get('/:id', (req, res) => {
   const desiredUser = users.find((user) => user._id === req.params.id);
-  if (desiredUser === 'undefined') {
+  if (!desiredUser) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
   } else {
     res.send(desiredUser);
